f4b9586307f2690f0f56ca130a30678435bca83f
Daved <daved@codemodus.com>
2016-12-29 17:36:50 -0800

    Practice json decoding.

---
+++ chuckjokes/simpledecoding/SimpleDecoding.elm
@@ -0,0 +1,38 @@
+module SimpleDecoding exposing (..)
+
+import Html exposing (..)
+import Json.Decode exposing (..)
+
+
+json : String
+json =
+    """
+{
+    "type": "success",
+    "value": {
+        "id": 490,
+        "joke": "Chuck Norris doesn't need to use AJAX because pages are too afraid to postback anyways.",
+        "categories": ["nerdy"]
+    }
+}
+    """
+
+
+decoder : Decoder String
+decoder =
+    at [ "value", "joke" ] string
+
+
+jokeRes : Result String String
+jokeRes =
+    decodeString decoder json
+
+
+main : Html msg
+main =
+    case jokeRes of
+        Ok joke ->
+            text joke
+
+        Err err ->
+            text err
