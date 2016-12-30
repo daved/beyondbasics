9cd8e47605a34d553b007502dd5c2eb2974c18d5
Daved <daved@codemodus.com>
2016-12-29 16:52:06 -0800

    Continue chuckjokes and add 'another' feature.

--- chuckjokes/Main.elm
+++ chuckjokes/Main.elm
@@ -1,6 +1,9 @@
 module Main exposing (..)
 
 import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Http
 
 
 -- model
@@ -15,19 +18,46 @@ initModel =
     "Finding a joke..."
 
 
+init : ( Model, Cmd Msg )
+init =
+    ( initModel, randomJoke )
+
+
+randomJoke : Cmd Msg
+randomJoke =
+    let
+        url =
+            "http://api.icndb.com/jokes/random"
+
+        req =
+            Http.getString url
+
+        cmd =
+            Http.send Joke req
+    in
+        cmd
+
+
 
 -- update
 
 
 type Msg
-    = Joke String
+    = Joke (Result Http.Error String)
+    | NewJoke
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Joke joke ->
-            joke
+        Joke (Ok joke) ->
+            ( joke, Cmd.none )
+
+        Joke (Err err) ->
+            ( (toString err), Cmd.none )
+
+        NewJoke ->
+            ( "fetching joke...", randomJoke )
 
 
 
@@ -36,13 +66,29 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div [] [ text model ]
+    div []
+        [ div []
+            [ text model ]
+        , div []
+            [ button
+                [ type_ "button"
+                , onClick NewJoke
+                ]
+                [ text "Another!" ]
+            ]
+        ]
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
 
 
 main : Program Never Model Msg
 main =
-    Html.beginnerProgram
-        { model = initModel
+    Html.program
+        { init = init
         , update = update
         , view = view
+        , subscriptions = subscriptions
         }
